ae343edadaa152370f7420e7585a1c1eba3abbb2
Morteza Tourani <morteza.tourani@gmail.com>
2015-07-31 17:19:19 +0430

    Widget Enhanced Widget.js

--- www/js/lib/Widget.js
+++ www/js/lib/Widget.js
@@ -2,30 +2,46 @@
 var Widget;
 (function () {
 	/**
+	*
 	 * [Widget normal implementations]
 	 * @method Widget
 	 * @param  {JSON} args
 	 * {
-	 * 		id: 		{String}	[ID of view]
-	 * 		class: 		{String}	[Name of class(es) (separate by space if more than one class)]
-	 * 		virtual: 	{Boolean}	[if true means "view not created yet!" and Widget will create element from given tag or div element]
-	 * 		tag: 		{String}	[Tag name of element if virtual]
+	 * 		id: 			{String}	[ID of view]
+	 * 		class: 			{String}	[Name of class(es) (separate by space if more than one class)]
+	 * 		virtual: 		{Boolean}	[if true means "view not created yet!" and Widget will create element from given tag or div element]
+	 * 		tag: 			{String}	[Tag name of element if virtual]
+	 * 		view: 			{JSON}		[View parameters]
+	 * 		{
+	 * 			attributes: {JSON}		[Element attributes]
+	 * 			data: 		{JSON}		[Attributes starting with "data-" like data-role]
+	 * 		}
 	 * }
-	 */
+	* 
+	*/
 	Widget = function Widget (args) {
 		args = args || {};
 
 		if (! (args.id || args.class))
-			throw "Widget received neither id or class name";
+			throw "Widget received neither id or class name (Widget.js)";
+		if (! args.role){
+			console.info(args);
+			throw "Widget.js:\t Widget role is ";
+		}
+
+		setProperty(args.deepChild("view.attributes"), args.deepVal("view.data"), { prefix: "data" });
+		args.deepChild("view.attributes")["data-role"] = args.role;
+		
 
 		var that = this,
 			virtual = args.virtual,
+			role = args.role,
 			contents = {},
-			tagName = args.tag || "div",
-			view = document.createElement(tagName);
+			view = (virtual) ? createElement(args.view) : {};
+
 		
 		view.id = args.id || "";
-		view.className = args.class || "";
+		view.className = (args.class || "").trim();
 
 		new Constants ({
 			template: args.template
@@ -42,11 +58,11 @@ var Widget;
 				var view = that.view();
 				var selector = "" + 
 					((view.id) ? '#' + view.id : '') + 
-					((view.className) ? '.' + view.className.split(" ").join(".") : '');
+					((view.className) ? '.' + view.className.trim().split(" ").join(".") : '');
 				return selector;
 			},
 			view: function (refresh) {
-				if (refresh)
+				if (refresh && !virtual || !view)
 					view = document.querySelector(that.selector());
 				return view;
 			},
@@ -60,8 +76,8 @@ var Widget;
 			},
 			data: function(){
 				if (arguments.length){
-					contents.Data = grabListFromArguments(arguments);
-					return that;
+					contents.Data = Object.create(grabListFromArguments(arguments));
+					return;
 				}
 				return contents.Data;
 			},
@@ -99,12 +115,41 @@ var Widget;
 		if (!virtual) 
 			window.addEventListener("load", function () {
 				view = document.querySelector(that.selector());
-				if (! view) 
-					throw "INVALID_ID_OR_CLASS!";
+				if (! view) {
+					throw "Widget.js Invalid id or class name!";
+				}
 				that.refresh();
 			});
 
 		new Fields ({ data: "array" }, contents);
 		DataObject.call(contents)
+	}
+
+	function createElement (args) {
+		args.tag = args.tag || "div";
+		if (!args.attributes || typeOf(args.attributes) !== typeOf({}))
+			args.attributes = {};
+
+		var el = document.createElement(args.tag);
+		for (var attr in args.attributes)
+			el.setAttribute (attr, args.attributes[attr]);
+
+		return el;
+	}
+
+	function setProperty (object, properties, options) {
+		if (properties) {
+			var prefix = "", postfix = "";
+
+			if (options && typeOf(options) == typeOf({})) {
+				if (options.prefix)
+					prefix = options.prefix + "-";
+				if (options.postfix)
+					postfix = "-" + options.postfix ;
+			};
+
+			for (var attr in properties)
+				(object[prefix + attr + postfix] = properties[attr]);
+		};
 	}
 })();
